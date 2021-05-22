6d96c0194ad0862417c0d48398e6feae41373000
Yeikzy <48528776+Yeikzy@users.noreply.github.com>
2021-05-22 23:15:07 +0200

    Add files via upload

---
+++ Discord Begginers/commands/yeikzy.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (client, message) => {
+    const user = message.author;
+    const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#eab3ff')
+	.setFooter('Yeikzy', 'https://i.imgur.com/Qs9Kczr.png')
+	.addFields(
+		{ name: 'Created by Yeikzy', value: '[Lien](https://discord.gg/hMa323uxdP)' },
+	)
+	.setTimestamp()
+	.setAuthor(`${user.username}`, `${user.avatarURL()}`);
+
+message.channel.send(exampleEmbed);
+
+};
+
+module.exports.help = {
+    name: 'Yeikzy',
+};
