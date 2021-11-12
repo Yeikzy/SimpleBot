999905b13fabc757d5b248d58d23cf78ed17a1f5
Yeikzy <48528776+Yeikzy@users.noreply.github.com>
2021-11-12 22:44:26 +0100

    Update help.js

--- Discord Begginers/commands/help.js
+++ Discord Begginers/commands/help.js
@@ -4,10 +4,12 @@ module.exports = {
     run: (client, message) => {
         message.channel.send(new Discord.MessageEmbed()
         .setTitle('⚙️ Résultat de votre commande :')
-        .setColor('#4baf70')
-        .addField('🛠 **Admin**', '`?ban`,`?kick`,`?mute`,`?unmute`')
-        .addField('🛡 **Sécuriter** ', '`?antiRaidon`,`?antiRaidoff`')
-        .addField('☎️ **Support**', '`?support`')
+        .setColor('#d30000')
+        .addField('🎉 **Fun** ', '`?avatar`,`?banner`')
+		.addField('🥤 **Love**', '`?kiss` , `?hug`')
+		.addField('📣 **Utility** ', '`?ping`,`?say`,`?botinfo`,`?clear`')
+		.addField('🛠 **Admin**', '`?ban`,`?kick`,`?warn`,`?unwarn`')
+        .addField('🏆 **Support us**', '`?twitter` , `?github`')
         .setFooter('By Yeikzy' , 'https://cdn.discordapp.com/attachments/653296971481612311/866109743332327444/yeikzypng.png'))
 
     },
